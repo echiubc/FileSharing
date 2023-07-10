05e5a27827196971ce6d8836e9ffceeb7d9ab596
Ethan Chiu <golferbc1@gmail.com>
2023-07-10 12:21:03 -0700

    login page completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
-# email:string
-# password_digest:string
-
-# password:string virtual
-# password_confirmation:string virtual
-
 class User < ApplicationRecord
     has_secure_password
-
-    validates :email, presence: true
 end
