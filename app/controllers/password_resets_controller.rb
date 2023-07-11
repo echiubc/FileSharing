9256690cba00681fe704cf30e7689993609a03b0
Ethan Chiu <golferbc1@gmail.com>
2023-07-11 16:14:14 -0700

    must fix error showing errors

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,14 +8,30 @@ class PasswordResetsController < ApplicationController
         if @user.present?
             # Send email
             PasswordMailer.with(user: @user).reset.deliver_now
-            redirect_to signin_path, notice: "If an account with that email was found, we have sent a link to reset your password."
+            
         end
+        redirect_to signin_path, notice: "If an account with that email was found, we have sent a link to reset your password."
     end
 
     def edit
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to signin_path, alert: "Your token has expired. Please try again."
     end
 
     def update
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to signin_path, notice: "Your password was reset successfully. Please sign in."
+        else
+            render :edit
+        end
     end
-    
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
