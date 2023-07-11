1eb33e740959b4d70d66427d38dcde19c33bc57d
Ethan Chiu <golferbc1@gmail.com>
2023-07-11 15:47:02 -0700

    password reset partially complete

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,21 @@
+class PasswordResetsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            # Send email
+            PasswordMailer.with(user: @user).reset.deliver_now
+            redirect_to signin_path, notice: "If an account with that email was found, we have sent a link to reset your password."
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+end
