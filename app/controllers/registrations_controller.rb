63376aca4a3c9d4fb15590cbe12f75781796bb66
Ethan Chiu <golferbc1@gmail.com>
2023-07-17 14:57:07 -0700

    redirect to posts when logged in/signed up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to root_path, notice: "Account successfully created!"
+            redirect_to posts_path, notice: "Account successfully created!"
         else
             render :new
         end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
         if user.present? && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to root_path, notice: "Logged in successfully!"
+            redirect_to posts_path, notice: "Logged in successfully!"
         else
             flash[:alert] = "Invalid email or password!"
             render :new
