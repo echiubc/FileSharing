7b9f1fca5ce9301b5930a03303e8bbc89fa06421
Ethan Chiu <golferbc1@gmail.com>
2023-08-04 13:38:12 -0700

    search works properly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,14 @@
 class Post < ApplicationRecord
     has_one_attached :file, dependent: :destroy
     belongs_to :user
-
+  
     def self.search(search)
-        if search
-            post_search = Post.find_by(filename: search)
-            if post_search
-                @posts = self.where(filename: search, user_id: Current.user.id)
-            else
-                @posts = self.where(user_id: Current.user.id)
-            end
-
-        else
-            @posts = self.where(user_id: Current.user.id)
-        end
+      if search
+        # Use ILIKE with '%' wildcard to perform a case-insensitive partial search on filename
+        search_term = "%#{search.downcase}%"
+        @posts = self.where("LOWER(filename) LIKE ?", search_term).where(user_id: Current.user.id)
+      else
+        @posts = self.where(user_id: Current.user.id)
+      end
     end
 end
